90e03c067065b6dbb322113256cc796d6db3cd76
whitenerdyswag <dominicrussoct@gmail.com>
2018-03-10 22:01:00 -0500

    Added current project files
    Added the SAOBootup project files as they were when I last updated them

---
+++ assets/Scripts/MovieController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovieController : MonoBehaviour {
+
+    public MovieTexture movie;
+    public GameObject Screen;
+    void Start()
+    {
+        GetComponent<Renderer>().material.mainTexture = movie;
+        movie.Play();
+        Screen.GetComponent<AudioSource>().Play();
+
+        Invoke("DoQuit", 20);
+	}
+
+    public void DoQuit()
+    {
+        Application.Quit();
+    }
+
+	void Update()
+    {
+        
+	}
+}
